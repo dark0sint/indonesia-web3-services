3461aa87fa1165f97d0518624cf2effac42c6d9e
DARK OSINT <levianawulandari7@gmail.com>
2025-09-20 10:00:20 +0700

    Create TaxPayment.js

---
+++ backend/models/TaxPayment.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const TaxPaymentSchema = new mongoose.Schema({
+    payer: { type: mongoose.Schema.Types.ObjectId, ref: 'User ', required: true },
+    amount: { type: Number, required: true },
+    txHash: { type: String, required: true },
+    status: { type: String, enum: ['Pending', 'Confirmed'], default: 'Pending' },
+}, { timestamps: true });
+
+module.exports = mongoose.model('TaxPayment', TaxPaymentSchema);
